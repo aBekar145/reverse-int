c7b2f18d332f881f6841a5f357da34232da1e978
Alex Gur <alexdream81@gmail.com>
2021-03-15 01:25:48 +0300

    feat: finish

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+if (n < 0) {
+    let plus = Math.abs(n);
+    let str = plus.toString().split('').reverse().join('');
+        let num = Number(str);
+        return num
+    } else {
+            let str = n.toString().split('').reverse().join('');
+        let num = Number(str);
+        return num;
+    }
 }
